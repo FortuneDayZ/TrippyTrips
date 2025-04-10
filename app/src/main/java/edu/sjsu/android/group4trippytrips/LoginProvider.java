557c70edf72c3ee86597d89e7b7b856106badfb9
Aung Paing Soe <aungpaing.soe@sjsu.edu>
2025-04-10 09:03:43 -0700

    Aung - Started LoginDB and its content provider

---
+++ app/src/main/java/edu/sjsu/android/group4trippytrips/LoginProvider.java
@@ -0,0 +1,60 @@
+package edu.sjsu.android.group4trippytrips;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+public class LoginProvider extends ContentProvider {
+
+    LoginDB db;
+
+    @Override
+    public boolean onCreate() {
+        db = new LoginDB(getContext());
+        return true;
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        long rowID = db.insert(values);
+        // If record is added successfully
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(uri, rowID);
+            Objects.requireNonNull(getContext()).getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
