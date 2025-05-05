681cc8f5abfecf17d34ddf02dd481f01c78972f0
FortuneDayZ <144715618+FortuneDayZ@users.noreply.github.com>
2025-05-04 19:04:42 -0700

    New UI test

--- app/src/main/java/edu/sjsu/android/group4trippytrips/WelcomePage.java
+++ app/src/main/java/edu/sjsu/android/group4trippytrips/WelcomePage.java
@@ -1,11 +1,15 @@
 package edu.sjsu.android.group4trippytrips;
 
 import android.os.Bundle;
+import android.text.Html; // Required for parsing HTML
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView; // Required for TextViews
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -16,18 +20,31 @@ public class WelcomePage extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_welcome_page, container, false);
+        return inflater.inflate(R.layout.fragment_welcome_page, container, false);
+    }
 
-        Button loginButton = view.findViewById(R.id.LogIn);
-        Button signUpButton = view.findViewById(R.id.signUp);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        loginButton.setOnClickListener(v ->
+        // Find Views
+        Button getStartedButton = view.findViewById(R.id.getStartedButton);
+        TextView welcomeText = view.findViewById(R.id.welcomeText);
+        TextView welcomeInstruction = view.findViewById(R.id.welcomeInstruction);
+
+        // Set Text for Headline (parsing HTML)
+        String welcomeHeadlineHtml = getString(R.string.welcome_headline);
+        welcomeText.setText(Html.fromHtml(welcomeHeadlineHtml, Html.FROM_HTML_MODE_LEGACY));
+
+        // Set Text for Instructions
+        welcomeInstruction.setText(R.string.welcome_instructions_detailed);
+
+        // Set Button Click Listener
+        getStartedButton.setOnClickListener(v ->
                 NavHostFragment.findNavController(WelcomePage.this)
                         .navigate(R.id.action_welcomePage_to_loginFragment)
         );
-
-        return view;
     }
 }
